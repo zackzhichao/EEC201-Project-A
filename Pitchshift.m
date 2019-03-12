54f0413d95e9481d26a79316ba7c2f1224096cc3
usrf123 <39092977+usrf123@users.noreply.github.com>
2019-03-12 11:23:38 -0700

    Update Pitchshift.m

--- Pitchshift.m
+++ Pitchshift.m
@@ -2,8 +2,17 @@
 % speed and pitch, then change the speed of speech.
 % If rate > 1, then move the pitch higher, otherwise lower
 % Assume shift by an integer number
-function So = Pitchshift(Speech,p,q,Fs)
-So = SOLA(Speech,p/q,Fs);
+function So = Pitchshift(Speech,rate,Fs)
+tem = rats(rate);
+k = strfing(tem,'/');
+if isempty(k)
+  p = rate;
+  q = 1;
+else
+  p = str2num(tem(1:k-1));
+  q = str2num(tem(k+1:end));
+end
+So = SOLA(Speech,rate,Fs);
 %% Change the speed and pitchshift by resampling
 So = resample(So,p,q);
 end
