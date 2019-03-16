b810eed5cf61b8caeeab950185c545bd141d98cc
zackzhichao <47911606+zackzhichao@users.noreply.github.com>
2019-03-15 20:09:51 -0700

    Add files via upload

--- TDPSOLAvocoder.m
+++ TDPSOLAvocoder.m
@@ -1,32 +1,27 @@
-%function TDPSOLAvocoder=TDPSOLAvocoder()
 function kkk=TDPSOLAvocoder()
 
 %setting parametres 
-global parametre
-parametre.TargetSignal='test_3ssingle.wav';    %wav file name to process
-parametre.Output='Psignal.wav';                %file name for output
-parametre.PitchScale=1;    %pitch scale parametre
-parametre.TimeScale=1;     %time scale parametre
-parametre.Cutoff=900;      %cut off frequency for lowpass
+TargetSignal='test_3ssingle.wav';    %wav file name to process
+Output='Psignal.wav';                %file name for output
+PitchScale=1;    %pitch scale parametre
+TimeScale=1;     %time scale parametre
+Cutoff=900;      %cut off frequency for lowpass
 
 %data to present
-global data
-data.OutputSignal=[];   %final result
-data.PitchMark=[];      %pitch mark information
-data.Candidate=[];      %pitch mark candidate to be analyzed
+OutputSignal=[];   %final result
 
-[Input,fs]=audioread(parametre.TargetSignal);
-Input=resample(Input, 160, 441);
-fs=fs*160/441;
-Input=Input-mean(Input);    %normalize the input signal
+[Input,fs]=audioread( TargetSignal );
+Input=Input-mean( Input );    %normalize the input signal
 
 %signal process part
-Input=Input.*fir1(48,parametre.Cutoff/fs,'low');   %Use lowpass to filter noise
-PitchContour=PitchEstimate(Input,fs);   %Calculate PitchContour
+LPSignal=Input.*fir1( 48 , Cutoff/fs , 'low' );   %Use lowpass to filter noise
+PitchContour=PitchEstimate( LPSignal , fs );   %Calculate PitchContour
 kkk=PitchContour;
-plot(kkk)
+DisplayPitchContour=PitchContour /max( PitchContour );
+plot(DisplayPitchContour)
 hold on
 plot(Input)
+hold off
 %audiowrite(config.Output,data.OuputSignal,fs);
 
 
